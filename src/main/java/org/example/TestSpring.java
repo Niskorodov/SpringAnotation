a06696add823a57e7f9bc1f2abcbd77d88f0490f
Dmitrii <103382610+Niskorodov@users.noreply.github.com>
2023-03-10 15:08:00 +0300

    Anotation Autowired

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,7 +8,7 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("rockMusic", Music.class);
+       /* Music music = context.getBean("rockMusic", Music.class);
 
         MusicPlayer musicPlayer = new MusicPlayer(music);
 
@@ -18,8 +18,11 @@ public class TestSpring {
 
         MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
 
-        musicPlayer.playMusic();
-
+        musicPlayer.playMusic();*/
+     //   MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+      //  musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
